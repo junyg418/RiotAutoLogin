c51bfcd2167c6f641d49a7917ec3b58899db686d
KIMYO9 <fgh33678@gmail.com>
2022-08-02 02:47:31 +0900

    2022-08-02 에러 gui 제작

--- GuiClassFile/PathErrorGui.py
+++ GuiClassFile/PathErrorGui.py
@@ -5,9 +5,44 @@ import sys
 class PathError(QWidget):
     def __init__(self):
         super().__init__()
-        self.
+        self.setGeometry(200, 200, 400, 100)
+        self.setFixedSize(400, 100)
+        self.setWindowTitle('클라이언트 경로 지정')
+
+        self.main_layout = QGridLayout()
+        self.riot_path_lineEdit = QLineEdit()
+        self.setPath_button = QPushButton('버튼')
+        self.close_button = QPushButton('확인')
+
+        self.init_ui()
+        self.init_widget()
+
+
+    def init_ui(self):
+        self.setLayout(self.main_layout)
+        self.main_layout.addWidget(self.riot_path_lineEdit, 0, 0, 2, 1)
+        self.main_layout.addWidget(self.setPath_button, 0, 1, 1, 1)
+        self.main_layout.addWidget(self.close_button, 1, 1, 1, 1)
+        self.main_layout.setRowStretch(1,1)
+    
+
+    def init_widget(self):
+        # ---- lineEdit
+        self.riot_path_lineEdit.setReadOnly(True)
+        # ---- setPath_Button
+        self.setPath_button.clicked.connect(self.findPath)
+
+    
+    def findPath(self):
+        '''
+        클라이언트 경로를 지정해주는 함수 -> setPath_button 누르면 호출
+        '''
+        file_path = QFileDialog.getOpenFileName(self, 'select Riot_Clint', 'C:/', filter='*.exe')
+        self.riot_path_lineEdit.setText(file_path[0])
+        # print(file_path[0])
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     widget = PathError()
-    sys.exit(app.exec_())
+    widget.show()
+    sys.exit(app.exec())

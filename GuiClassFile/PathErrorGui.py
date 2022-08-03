b51cbcf3a7f9a3e56874425738bf6fc40339f0ba
KIMYO9 <fgh33678@gmail.com>
2022-08-03 21:40:47 +0900

    close_button 이벤트함수 추가 PathErrorGui.py 닫을떄 이벤트 경로 이상 추가

--- GuiClassFile/PathErrorGui.py
+++ GuiClassFile/PathErrorGui.py
@@ -1,4 +1,6 @@
+import PySide6
 from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt
 import sys
 
 
@@ -11,6 +13,7 @@ class PathError(QWidget):
 
         self.main_layout = QGridLayout(self)
         self.riot_path_lineEdit = QLineEdit()
+        self.status_display_label = QLabel('라이엇 클라이언트 경로를 설정해주세요')
         self.setPath_button = QPushButton('설정')
         self.close_button = QPushButton('확인')
 
@@ -19,12 +22,18 @@ class PathError(QWidget):
 
     def init_ui(self):
         self.setLayout(self.main_layout)
-        self.main_layout.addWidget(self.riot_path_lineEdit, 0, 0, 2, 1)
+
+        self.main_layout.addWidget(self.status_display_label, 0, 0, 1, 1)
         self.main_layout.addWidget(self.setPath_button, 0, 1, 1, 1)
+
+        self.main_layout.addWidget(self.riot_path_lineEdit, 1, 0, 1, 2)
         self.main_layout.addWidget(self.close_button, 1, 1, 1, 1)
+        self.main_layout.setColumnStretch(0,2)
         self.main_layout.setRowStretch(1, 1)
 
     def init_widget(self):
+        # ---- Label
+        self.status_display_label.setAlignment(Qt.AlignCenter)
         # ---- lineEdit
         self.riot_path_lineEdit.setReadOnly(True)
         # ---- setPath_Button
@@ -40,8 +49,11 @@ class PathError(QWidget):
         self.riot_path_lineEdit.setText(file_path[0])
 
     def page_close(self):
-        self.path_save()
-        self.close()
+        if 'RiotClientServices.exe' in self.riot_path_lineEdit.text().split('/'):
+            self.path_save()
+            self.close()
+        else:
+            self.status_display_label.setText('경로가 잘못되었습니다')
 
     def path_save(self) -> None:
         """
@@ -51,7 +63,8 @@ class PathError(QWidget):
         print(file_path)  # TODO 로컬파일 저장으로 바꾸기
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+def open_error_page():
     app = QApplication(sys.argv)
     widget = PathError()
     widget.show()
--- RiotRun.py
+++ RiotRun.py
@@ -1,8 +1,9 @@
 import subprocess
+from GuiClassFile import PathErrorGui
 
-riot_clint_path = "C:\Riot Games\Riot Client\RiotClientServices.exe"
+riot_clint_path = "C:\Riot Games\Riot Client\RiotClienServices.exe"
 
 try:
     subprocess.check_output(riot_clint_path, shell=True)
 except:
-    print('경로 이상')  # TODO 경고 매세지 추가 예정
+    PathErrorGui.open_error_page()

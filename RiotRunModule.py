7f2f9b09141a26598b58a59084d2786d465b688a
KIMYO9 <fgh33678@gmail.com>
2022-08-09 23:39:28 +0900

    RiotRunModule.py getpath 기능 연동 및 함수화

--- RiotRunModule.py
+++ RiotRunModule.py
@@ -1,19 +1,17 @@
 import subprocess
+from CsvDataProcessingModule import get_path
 from GuiClassFile import PathErrorGui
 
-
-# def run_riot_clint(path):
-#     try:
-#         subprocess.check_output(path, shell=True)
-#     except:
-#         PathErrorGui.open_error_page()
 riot_clint_path = "C:\Riot Games\Riot Client\RiotClientServices.exe"
-riot_clint_path = "C:\Riot Games\Riot Client\RiotClientService.exe"
-def run():
-    # try:
-    subprocess.check_output(riot_clint_path, shell=True)
-    # except:
-    #     PathErrorGui.open_error_page()
+
+
+def run_riot_clint() -> None:
+    path = get_path()  # import CsvDataProcessingModule
+    try:
+        subprocess.check_output(path, shell=True)
+    except subprocess.CalledProcessError:
+        PathErrorGui.open_error_page()
+
 
 if __name__ == '__main__':
-    run()
+    run_riot_clint()

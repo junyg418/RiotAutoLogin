0a4f2948420eeea0e96a1c299bb8f2f8d959c94d
KIMYO9 <fgh33678@gmail.com>
2022-08-12 13:20:02 +0900

    CsvDataProcessingModule.py MainGui 사용 함수 3개 제작 및 TODO 추가

--- CsvDataProcessingModule.py
+++ CsvDataProcessingModule.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# TODO :사용된 파일에 따라서 불러와지는 df 다르게 변환
+
+# --------------- RiotRunModule ---------------
 default_value_df = pd.read_csv('./csv_file/defaultValueCsvData.csv', header=None, names=['name', 'data'])
 default_value_df.set_index('name', inplace=True)
 
@@ -25,3 +28,46 @@ def get_path() -> str:
     """
     path = default_value_df.loc['riot_path', 'data']
     return path
+
+
+# --------------- MainGui ---------------
+account_df = pd.read_csv('./csv_file/accountCsvData.csv', sep='/')
+
+
+def get_len_account() -> int:
+    """
+    계정의 개수 return 하는 함수
+    :return:
+        account_len:int -> 계정의 개수
+    """
+    account_len = len(account_df)
+    return account_len
+
+
+def df_id_to_list() -> list:
+    """
+    계정들의 아이디를 리스트로 반환해주는 함수
+    :return:
+        list -> 계정 id list
+    """
+    id_df = account_df['id']
+    id_list = id_df.tolist()
+    return id_list
+
+
+def select_idx_to_id(idx: int) -> 'str':
+    """
+    선택된 id 반환 함수
+    :param idx:
+        index : int -> 아이디의 인덱스
+    :return:
+        id, password : tuple
+    """
+    account_value_df = account_df.iloc[idx]
+    id_value = str(account_value_df.id)
+    pw_value = str(account_value_df.password)
+    return id_value, pw_value
+
+
+if __name__ == '__main__':
+    select_idx_to_id(1)

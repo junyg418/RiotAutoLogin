7bbab52e9f80aad5350f4d1c75f86202f102f3e6
KIMYO9 <fgh33678@gmail.com>
2022-08-28 23:24:03 +0900

    라벨 추가 및 함수 제거

--- GuiClassFile/AccountAddGui.py
+++ GuiClassFile/AccountAddGui.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication ,QDialog, QGridLayout, QLineEdit, QPushButton
+from PySide6.QtWidgets import QApplication ,QDialog, QGridLayout, QLineEdit, QPushButton, QLabel
 
 class Account(QDialog):
     def __init__(self):
@@ -8,17 +8,23 @@ class Account(QDialog):
         self.main_layout = QGridLayout(self)
         self.setLayout(self.main_layout)
 
-        self.id_lineedit = QLineEdit('id')
-        self.pw_lineedit = QLineEdit('pw')
+        self.id_label = QLabel('Id')
+        self.pw_label = QLabel('Pw')
+        self.id_lineedit = QLineEdit()
+        self.id_lineedit.displayText()
+        self.pw_lineedit = QLineEdit()
         
         self.accept_button = QPushButton('저장')
         self._init_ui()
         self.show()
     
     def _init_ui(self):
-        self.main_layout.addWidget(self.id_lineedit, 0, 0)
-        self.main_layout.addWidget(self.pw_lineedit, 1, 0)
-        self.main_layout.addWidget(self.accept_button, 0, 1)
+        self.main_layout.addWidget(self.id_label, 0, 0)
+        self.main_layout.addWidget(self.pw_label, 1, 0)
+
+        self.main_layout.addWidget(self.id_lineedit, 0, 1)
+        self.main_layout.addWidget(self.pw_lineedit, 1, 1)
+        self.main_layout.addWidget(self.accept_button, 0, 2)
 
 def open_account_append():
     app = QApplication(sys.argv)

540f289ea0249a10b29abad4192b7120287fe941
KIMYO9 <fgh33678@gmail.com>
2022-09-02 15:39:02 +0900

    AccountAddGui 닫을 때 호출 함수 제작

--- GuiClassFile/AccountAddGui.py
+++ GuiClassFile/AccountAddGui.py
@@ -1,9 +1,15 @@
 import sys
-from PySide6.QtWidgets import QApplication ,QDialog, QGridLayout, QLineEdit, QPushButton, QLabel
+import os
+from PySide6.QtWidgets import QApplication, QDialog, QGridLayout, QLineEdit, QPushButton, QLabel
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from CsvDataProcessingModule import add_account
+
 
 class Account(QDialog):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('계정 추가')
 
         self.main_layout = QGridLayout(self)
         self.setLayout(self.main_layout)
@@ -12,11 +18,12 @@ class Account(QDialog):
         self.pw_label = QLabel('Pw')
         self.id_lineedit = QLineEdit()
         self.pw_lineedit = QLineEdit()
-        
+
         self.accept_button = QPushButton('저장')
+        self.accept_button.clicked.connect(self.clicked_accept_button)
         self._init_ui()
         self.show()
-    
+
     def _init_ui(self):
         self.main_layout.addWidget(self.id_label, 0, 0)
         self.main_layout.addWidget(self.pw_label, 1, 0)
@@ -24,6 +31,18 @@ class Account(QDialog):
         self.main_layout.addWidget(self.id_lineedit, 0, 1)
         self.main_layout.addWidget(self.pw_lineedit, 1, 1)
         self.main_layout.addWidget(self.accept_button, 0, 2)
+
+    def clicked_accept_button(self):
+        """
+        accept_button 클릭시 호출
+        클릭시 accountCsvData.csv 에 입력된 id, pw 를 추가하는 함수
+        :return:
+            Dialog 닫기
+        """
+        account_id = self.id_lineedit.text()
+        account_pw = self.pw_lineedit.text()
+        add_account(account_id, account_pw)
+        return self.close()
 
 
 if __name__ == '__main__':

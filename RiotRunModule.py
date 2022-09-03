b1aa8515dc9334f1e13df84ea16e9f19d7da8bff
KIMYO9 <fgh33678@gmail.com>
2022-09-03 21:40:02 +0900

    기존 run 함수 제거 및 os 모듈을 사용하여 다시 제작

--- RiotRunModule.py
+++ RiotRunModule.py
@@ -1,15 +1,31 @@
-import subprocess
+import os
 from CsvDataProcessingModule import get_path
 from GuiClassFile import PathErrorGui
 
-riot_clint_path = "C:\Riot Games\Riot Client\RiotClientServices.exe"
 
+# riot_clint_path = "C:\Riot Games\Riot Client\RiotClientServices.ex"
+
+# 기존 run 함수 제거
+# > 사유: 프로그램이 종료됨과 상관없이 실행되야하는데 subprocess 모듈과는 알맞지 않기에 변경함
+# def run() -> None:
+#     """
+#     라이엇 클라이언트 실행시키는 함수  -> subprocess 모듈 사용
+#     """
+#     path = get_path()  # import CsvDataProcessingModule
+#     try:
+#         subprocess.run(path, shell=True)
+#         # subprocess.check_output(path, shell=True)
+#     except subprocess.CalledProcessError:
+#         PathErrorGui.open_error_page()
 
 def run() -> None:
-    path = get_path()  # import CsvDataProcessingModule
+    """
+    라이엇 클라이언트 실행시키는 함수
+    """
+    path = get_path()
     try:
-        subprocess.check_output(path, shell=True)
-    except subprocess.CalledProcessError:
+        os.startfile(path)
+    except FileNotFoundError:
         PathErrorGui.open_error_page()
 
 
